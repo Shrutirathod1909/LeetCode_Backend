48c2bbd504a9aa62d9dfbaba90ae733991e953b2
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-29 09:42:11 -0500

    Update validator.js

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,22 +1,21 @@
-const validator = require("validator");
+const validator =require("validator");
 
-const validate = (data) => {
-  const mandatoryField = ["firstName", "emailId", "password"];
+// req.body 
 
-  const isAllowed = mandatoryField.every(k =>
-    Object.keys(data).includes(k)
-  );
+const validate = (data)=>{
+   
+    const mandatoryField = ['firstName',"emailId",'password'];
 
-  if (!isAllowed)
-    throw new Error("Some fields are missing");
+    const IsAllowed = mandatoryField.every((k)=> Object.keys(data).includes(k));
 
-  if (!validator.isEmail(data.emailId))
-    throw new Error("Invalid email");
+    if(!IsAllowed)
+        throw new Error("Some Field Missing");
 
-  if (!validator.isStrongPassword(data.password))
-    throw new Error("Weak password"); // ✅ FIXED TYPO
+    if(!validator.isEmail(data.emailId))
+        throw new Error("Invalid Email");
 
-  return true; // ✅ GOOD PRACTICE
-};
+    if(!validator.isStrongPassword(data.password))
+        throw new Error("Week Password");
+}
 
 module.exports = validate;
