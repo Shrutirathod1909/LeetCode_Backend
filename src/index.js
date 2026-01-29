e6365798bcb5c8a381dfc0e464ba59e83639d815
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-29 09:42:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,49 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 require('dotenv').config();
-const main = require('./config/db');        // MongoDB connection
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const redisClient = require('./config/redis');
-
-// Routers
+const main =  require('./config/db')
+const cookieParser =  require('cookie-parser');
 const authRouter = require("./routes/userAuth");
+const redisClient = require('./config/redis');
 const problemRouter = require("./routes/problemCreator");
-const submitRouter = require("./routes/submit");
-const aiRouter = require("./routes/aiChatting");
+const submitRouter = require("./routes/submit")
+const aiRouter = require("./routes/aiChatting")
 const videoRouter = require("./routes/videoCreator");
-app.set("trust proxy", 1)
-// Middleware
+const cors = require('cors')
+
+// console.log("Hello")
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+    origin: 'http://localhost:5173',
+    credentials: true 
+}))
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
-app.use('/user', authRouter);
-app.use('/problem', problemRouter);
-app.use('/submission', submitRouter);
-app.use('/ai', aiRouter);
-app.use('/video', videoRouter);
+app.use('/user',authRouter);
+app.use('/problem',problemRouter);
+app.use('/submission',submitRouter);
+app.use('/ai',aiRouter);
+app.use("/video",videoRouter);
 
 
-// Test route
-app.get("/apitesting", (req, res) => {
-    res.send("Backend running 🚀");
-});
+const InitalizeConnection = async ()=>{
+    try{
 
-// Error handling middleware (optional but recommended)
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: err.message });
-});
+        await Promise.all([main(),redisClient.connect()]);
+        console.log("DB Connected");
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening at port number: "+ process.env.PORT);
+        })
 
-// Initialize DB & Redis and start server
-const InitializeConnection = async () => {
-  try {
-    await main();
-    if (!redisClient.isOpen) {
-      await redisClient.connect();
     }
+    catch(err){
+        console.log("Error: "+err);
+    }
+}
 
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () =>
-      console.log("Server listening on port " + PORT)
-    );
-  } catch (err) {
-    console.error("Initialization Error:", err);
-  }
-};
 
-InitializeConnection();
+InitalizeConnection();
+
