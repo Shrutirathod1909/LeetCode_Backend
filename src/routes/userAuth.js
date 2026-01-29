319ca0ad23fde9a9d32d15bb45422dbd5cdf7c66
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-29 09:40:59 -0500

    Update userAuth.js

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,41 +1,36 @@
-const express = require("express");
-const authRouter = express.Router();
+const express = require('express');
 
-const {
-  register,
-  login,
-  logout,
-  adminRegister,
-  deleteProfile,
-  adminLogin,
-} = require("../controllers/userAuthent");
-
+const authRouter =  express.Router();
+const {register, login,logout, adminRegister,deleteProfile} = require('../controllers/userAuthent')
 const userMiddleware = require("../middleware/userMiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware");
+const adminMiddleware = require('../middleware/adminMiddleware');
 
-// User auth
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.post("/logout", userMiddleware, logout);
+// Register
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', userMiddleware, logout);
+authRouter.post('/admin/register', adminMiddleware ,adminRegister);
+authRouter.delete('/deleteProfile',userMiddleware,deleteProfile);
+authRouter.get('/check',userMiddleware,(req,res)=>{
 
-// Admin auth
-authRouter.post("/admin/login", adminLogin);
-authRouter.post("/admin/register", adminMiddleware, adminRegister);
+    const reply = {
+        firstName: req.result.firstName,
+        emailId: req.result.emailId,
+        _id:req.result._id,
+        role:req.result.role,
+    }
 
-// User profile
-authRouter.delete("/deleteProfile", userMiddleware, deleteProfile);
+    res.status(200).json({
+        user:reply,
+        message:"Valid User"
+    });
+})
+// authRouter.get('/getProfile',getProfile);
 
-// Token check
-authRouter.get("/check", userMiddleware, (req, res) => {
-  res.status(200).json({
-    user: {
-      firstName: req.user.firstName,
-      emailId: req.user.emailId,
-      _id: req.user._id,
-      role: req.user.role,
-    },
-    message: "Valid User",
-  });
-});
 
 module.exports = authRouter;
+
+// login
+// logout
+// GetProfile
+
