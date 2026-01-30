99a2fa54f458e77390bec415b338f58ae3ffa61c
Shrutirathod1909 <shrutirathod1909@gmail.com>
2026-01-30 08:34:59 -0500

    Update CORS origin URL for the application

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'https://leetmaster.netlify.app',
+    origin: 'https://leetmasters.netlify.app',
     credentials: true 
 }))
 
